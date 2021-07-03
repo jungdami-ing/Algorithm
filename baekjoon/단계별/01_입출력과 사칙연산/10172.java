6b2233e129a0b3497dab52ec3111d653ed7653f8
Dami Jung <jungdamiing@gmail.com>
2021-07-04 07:02:27 +0900

    4단계

---
+++ baekjoon/단계별/01_입출력과 사칙연산/10172.java
@@ -0,0 +1,25 @@
+//4. 개
+//주어진 예제처럼 출력하는 문제 4
+
+//문제
+//아래 예제와 같이 고양이를 출력하시오.
+//고양이를 출력한다.
+/**
+
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+
+*/
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+	System.out.println("( 0 )\"\"\"\\");
+	System.out.println("|\"^\"`    |");
+	System.out.println("||_/=\\\\__|");
+  }
+}
