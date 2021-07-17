546c916bdd58539d31f25fe22b04baf6742fc3bc
Dami Jung <jungdamiing@gmail.com>
2021-07-17 17:37:42 +0900

    6단계

--- baekjoon/단계별/05_1차원 배열/8958.java
+++ baekjoon/단계별/05_1차원 배열/8958.java
@@ -25,3 +25,36 @@
 // 7
 // 55
 // 30
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);	
+		String ox[] = new String[sc.nextInt()];
+		
+		
+		for (int i = 0; i < ox.length; i++) {
+			ox[i] = sc.next();
+		}
+		
+		for (int i = 0; i < ox.length; i++) {
+			int cnt = 0;
+			int sum = 0;
+			
+			for (int j = 0; j < ox[i].length(); j++) {
+				if (ox[i].charAt(j) == 'O') {
+					cnt++;
+				} else {
+					cnt = 0;
+				}
+				
+				sum += cnt;
+			}
+			
+			System.out.println(sum);
+		}
+
+	}
+}
